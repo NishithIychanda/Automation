9025967976f9ba2c8d3aef71d827e588b4832aee
nishithin <nishith.in@jai-kisan.com>
2022-01-13 17:29:38 +0530

    Login in failure case handled

---
+++ Object Repository/Dashboard Login/Page_Jai Kisan Applications/Jai Kisan Logo.rs
@@ -0,0 +1,66 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Jai Kisan Logo</name>
+   <tag></tag>
+   <elementGuidId>92b0627e-0433-4fc1-893d-8f6a6373632c</elementGuidId>
+   <imagePath></imagePath>
+   <selectorCollection>
+      <entry>
+         <key>IMAGE</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>img[alt=&quot;Jai Kisan Logo&quot;]</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//body/app-root[1]/section[1]/app-side-nav-new[1]/div[1]/div[1]/img[1]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>img</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>alt</name>
+      <type>Main</type>
+      <value>Jai Kisan Logo</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>src</name>
+      <type>Main</type>
+      <value>../../../assets/Turban80.png</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[1]/app-root[1]/section[@class=&quot;rbac-container&quot;]/app-side-nav-new[1]/div[@class=&quot;side-nav-container&quot;]/div[@class=&quot;brand&quot;]/img[1]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:img</name>
+      <type>Main</type>
+      <value>//img[@alt='Jai Kisan Logo']</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//img</value>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Scripts/Dashboard/Common Cases/Login Failure test/Script1642057532549.groovy
@@ -0,0 +1,64 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://test-dashboard.jai-kisan.com/home')
+
+WebUI.maximizeWindow()
+
+WebUI.setText(findTestObject('Dashboard Login/Login_email'), 'nishith.in@jai-kisan.com')
+
+WebUI.setEncryptedText(findTestObject('Dashboard Login/Login_password'), 'HSbZGfHepnKXpMYblUbvtA==')
+
+WebUI.click(findTestObject('Dashboard Login/button_login'))
+
+//Output = WebUI.verifyElementPresent(findTestObject('Dashboard Login/Page_Jai Kisan Applications/Jai Kisan Logo'), 2)
+
+String currentUrl = WebUI.getUrl()
+System.println(currentUrl)
+
+
+
+Date today = new Date()
+String todaysDate = today.format('MM_dd_yy');
+String nowTime = today.format('hh_mm_ss');
+
+if (currentUrl == 'https://test-dashboard.jai-kisan.com/home') {
+    
+    System.println('LOGIN SUCCESSFUL')
+	WebUI.takeScreenshot("E:\\Automation\\Screenshots\\OutputScreenshots\\screenshot_"+ todaysDate +"-" + nowTime +".PNG")
+	
+} else {
+	
+    WebUI.takeScreenshot("E:\\Automation\\Screenshots\\OutputScreenshots\\screenshot_"+ todaysDate +"-" + nowTime +".PNG")
+
+    WebUI.verifyElementPresent(findTestObject('Dashboard Login/Text_Invalid username or password'), 2)
+
+    Message = WebUI.getText(findTestObject('Dashboard Login/Text_Invalid username or password'))
+
+    System.println(Message)
+}
+
+
+
+
+
+
+
