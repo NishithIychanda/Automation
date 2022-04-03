a408edab40cffc7d039295db5b8364f01f0af070
Nishith IN <nishith.in@jai-kisan.com>
2022-04-03 12:23:22 +0530

    Upload store documents -invalid file formats case

---
+++ Object Repository/Dashboard/Entity/Network/Store Details List/Upload Store Documents/FileUpload_Address Proof Upload.gif.rs
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>FileUpload_Address Proof Upload.gif</name>
+   <tag></tag>
+   <elementGuidId>210a70eb-493c-4f06-86e8-04a151b13dd7</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//input[@id= 'Address Proof Upload.gif']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
