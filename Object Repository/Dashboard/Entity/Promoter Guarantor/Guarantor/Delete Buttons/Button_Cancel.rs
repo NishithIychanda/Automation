75304198230a11d11b89fee807158e3fc8d4a065
Nishith IN <nishith.in@jai-kisan.com>
2022-04-03 13:31:37 +0530

    Guarantor Delete case

---
+++ Object Repository/Dashboard/Entity/Promoter Guarantor/Guarantor/Delete Buttons/Button_Cancel.rs
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Button_Cancel</name>
+   <tag></tag>
+   <elementGuidId>b23dc5ee-3312-4925-8e81-f561fd73c931</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//button[@id='cancelbtn']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Object Repository/Dashboard/Entity/Promoter Guarantor/Guarantor/Delete Buttons/Close Icon.rs
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Close Icon</name>
+   <tag></tag>
+   <elementGuidId>32af86ca-4fbb-41b7-9967-0d7e9c8e0d32</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//mat-icon[contains(text(),'close')]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
