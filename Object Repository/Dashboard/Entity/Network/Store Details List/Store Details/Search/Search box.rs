4ae4e57a3ac03c45f08b2f830fa54f1ad922be47
Nishith IN <nishith.in@jai-kisan.com>
2022-04-03 13:22:12 +0530

    Promoter Delete Case

---
+++ Object Repository/Dashboard/Entity/Network/Store Details List/Store Details/Search/Search box.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Search box</name>
+   <tag></tag>
+   <elementGuidId>80af95da-3c21-4082-a95c-e7b31c23ba38</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//mat-tab-body/div[1]/div[1]/anchors-section-renderer[1]/div[1]/anchors-content-decider[2]/anchors-member-details[1]/div[1]/anchors-table[1]/div[1]/div[2]/div[1]/anchors-table-search[1]/section[1]/form[1]/mat-form-field[1]/div[1]/div[1]/div[2]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Object Repository/Dashboard/Entity/Promoter Guarantor/Promoter/Delete Buttons/Delete Icon.rs
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Delete Icon</name>
+   <tag></tag>
+   <elementGuidId>6c1d7386-5647-42f4-9dac-95c6b48f7e3d</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//mat-icon[contains(text(),'delete_outline')]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
