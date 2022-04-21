c185a48de6b4417fd3c2992a192ae6f8ff2a0779
Nishith IN <nishith.in@jai-kisan.com>
2022-04-21 19:19:12 +0530

    Loan proposal search using Product ID

---
+++ Object Repository/Dashboard/Entity/Loan Proposals/Loan Proposal Search/Proposal ID.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Proposal ID</name>
+   <tag></tag>
+   <elementGuidId>f16b1315-5f87-4d01-8190-58e33df82c7f</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//tbody/tr[1]/td[1]</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
