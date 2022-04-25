0de924b56a61b6a6267cca7018db68ae65299369
Nishith IN <nishith.in@jai-kisan.com>
2022-04-25 12:17:14 +0530

    Make primary in company info-Bharma

---
+++ Scripts/Dashboard/Entity/Company Information/Make Primary Bank details/Script1650635321155.groovy
@@ -0,0 +1,34 @@
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+
+WebUI.callTestCase(findTestCase('Dashboard/Common Cases/Dashboard Login'), [:], FailureHandling.STOP_ON_FAILURE)
+
+WebUI.setText(findTestObject('Dashboard/Entity/Common/search bar'), 'suryo')
+
+WebUI.delay(4)
+
+WebUI.click(findTestObject('Dashboard/Entity/Common/div_Entity'))
+
+WebUI.click(findTestObject('Dashboard/Entity/Company Information/Bank_Details/Buttons/Make Primary Button'))
+
+Toastmessge = WebUI.getText(findTestObject('Dashboard/Entity/Company Information/Buisness_Details/Toast Messages/Toast Message'), 
+    FailureHandling.STOP_ON_FAILURE)
+
+System.println(Toastmessge)
+
