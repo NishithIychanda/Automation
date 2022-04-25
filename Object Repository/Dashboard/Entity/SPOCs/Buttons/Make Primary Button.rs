778e021dce19d49ef5ba88f3638b03aea55c9d32
Nishith IN <nishith.in@jai-kisan.com>
2022-04-25 13:30:42 +0530

    Spoc Make Primary-Bharma

---
+++ Object Repository/Dashboard/Entity/SPOCs/Buttons/Make Primary Button.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Make Primary Button</name>
+   <tag></tag>
+   <elementGuidId>3a27877d-7533-47ba-b63c-b39ffb8f10ce</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//*[@id=&quot;cdk-drop-list-1&quot;]/tbody/tr[4]/td[4]/div/div/anchors-table-button/div/div/button</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
