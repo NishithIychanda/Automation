3c642e388897ef6368db31a926d1a930106ccb94
Nishith IN <nishith.in@jai-kisan.com>
2022-05-06 16:29:17 +0530

    Search with Proposal Name

---
+++ Object Repository/Dashboard/Entity/Loan Proposals/Loan Proposal Search/ProposalName.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>ProposalName</name>
+   <tag></tag>
+   <elementGuidId>8663d792-676a-455a-9ef8-2d39f04936e0</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//tbody/tr[1]/td[2]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
